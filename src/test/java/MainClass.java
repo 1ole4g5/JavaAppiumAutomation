8d433217fcda6faddf6391d5bb9403d8ffaaadbe
Мартыненко Олег Викторович <1ole4g5@gmail.com>
2025-02-11 12:13:45 +0300

    Тест 2.

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -1,8 +1,13 @@
 public class MainClass {
 
+    private int class_number = 20;
     int number = 14;
 
     public int getLocalNumber() {
         return number;
+    }
+
+    public int getClassNumber() {
+        return class_number;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,14 +1,22 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest {
 
     int actual = new MainClass().getLocalNumber();
     int expected = 14;
+    int secondActual = new MainClass().getClassNumber();
+    int secondExpected = 45;
 
     @Test
     public void testGetLocalNumber() {
         assertEquals(actual, expected, "Число " + actual + " не равно числу " + expected);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        assertTrue(secondActual > secondExpected, "Число " + secondActual + " не больше числа " + secondExpected);
     }
 }
